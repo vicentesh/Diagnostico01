a297d05def38adfad131b74f350c28120727079c
vicentesh <48304636+vicentesh@users.noreply.github.com>
2022-08-16 12:44:17 -0400

    esqueletos creados

---
+++ main.py
@@ -0,0 +1,21 @@
+def mostRetweeted():
+    print("hola1")
+    
+def mostActiveUsers():
+    print("hola2")
+
+def mostTweetsDay():
+    print("hola3")
+
+def trendingTopics():
+    print("hola4")
+
+# main function
+def main():
+    mostRetweeted()
+    mostActiveUsers()
+    mostTweetsDay()
+    trendingTopics()
+
+if __name__ == '__main__':
+    main()
